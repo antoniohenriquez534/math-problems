4012731de4de92b15d1abac8407ee1f61e6cac70
antoniohenriquez534 <antoniohenriquez534@gmail.com>
2025-05-17 08:29:16 +0000

    Add 35.rs

---
+++ src/35.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // This is an example of how to solve the problem described.
+    let x = 5;
+    let y = 10;
+
+    if x > y {
+        println!("x is greater than y");
+    } else {
+        println!("y is greater than x");
+    }
+}
